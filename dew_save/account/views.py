479cbd860322df364c4a8cbf38522b485e4142b5
atsushi-takahashi <67621987+atsushi-takahashi-0306@users.noreply.github.com>
2020-08-04 18:51:56 +0900

    signupページの作成
    Userモデルはデフォルトのものを使用したのでモデル作成に関するコードの記述はなし（もう既にadminページにある）。
    ログインページより先にsignupページを作成した。エラー文も簡易だが表示される。フォームから入力してDBにレコードが作成されるのを確認済み。

---
+++ dew_save/account/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
+
+
+
+class SignUp(CreateView):
+    form_class = SignUpForm
+    template_name = "account/signup.html" 
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save() 
+        login(self.request, user)
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url())
