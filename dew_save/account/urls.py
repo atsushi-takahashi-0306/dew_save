8bd0a3ff8311e1fdcd5f22bc26c0c07ad6c28d8a
atsushi-takahashi <67621987+atsushi-takahashi-0306@users.noreply.github.com>
2020-08-06 04:36:42 +0900

    アカウントページの作成
    アカウントページを作成し、削除、更新できるようにした。DBも更新、削除されていることを確認済み。
    アップデート後のリダイレクト先がまだ上手く行っていない。

--- dew_save/account/urls.py
+++ dew_save/account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUp
 from django.contrib.auth import views as auth_views
+from .views import Delete_user
+from .views import Update_user
 
 
 app_name = 'account'
@@ -9,4 +11,6 @@ urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('delete_user/<int:pk>/', Delete_user.as_view(), name='delete'),
+    path('update_user/<int:pk>/', Update_user.as_view(), name='update'),
 ]
--- dew_save/account/views.py
+++ dew_save/account/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from .forms import SignUpForm
-
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.shortcuts import redirect
 
 
 class SignUp(CreateView):
@@ -16,3 +20,16 @@ class SignUp(CreateView):
         login(self.request, user)
         self.object = user 
         return HttpResponseRedirect(self.get_success_url())
+
+class Delete_user(UserPassesTestMixin,DeleteView):
+    model = User
+    template_name = 'account/account.html'
+    success_url = reverse_lazy('wine:home')
+
+class Update_user(UpdateView):
+    model = User
+    form_class = SignUpForm
+    template_name = 'account/account.html'
+    def get_success_url(self):
+        args=self.object.id
+        return reverse_lazy('account:update', args=(self.object.id,))
