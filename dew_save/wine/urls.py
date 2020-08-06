f2d7533f81c03ef063272760684f3eb219da6d41
atsushi-takahashi <67621987+atsushi-takahashi-0306@users.noreply.github.com>
2020-08-06 16:51:07 +0900

    ワインポストの詳細ページ作成
    ワインポストの写真をクリックするとワイン詳細ページにジャンプ。以前はこちらでも編集、削除出来たが詳細ページではただ情報を載せるだけにした。マイワインのページのみ編集、削除可能。

--- dew_save/wine/urls.py
+++ dew_save/wine/urls.py
@@ -3,6 +3,7 @@ from .import views
 from .views import My_wine
 from .views import Add_wine
 from .views import All_wine
+from .views import Detail_wine
 from .views import Delete_wine
 from .views import Update_wine
 # imege file用↓
@@ -18,6 +19,7 @@ urlpatterns = [
     path('my_wine/', My_wine.as_view(), name='my'),
     path('add_wine/', Add_wine.as_view(), name='add'),
     path('all_wine/', All_wine.as_view(), name='all'),
+    path('detail_wine/<int:pk>/', Detail_wine.as_view(), name='detail'),
     path('delete_wine/<int:pk>/', Delete_wine.as_view(), name='delete'),
     path('update_wine/<int:pk>/', Update_wine.as_view(), name='update'),
 ]
--- dew_save/wine/views.py
+++ dew_save/wine/views.py
@@ -4,6 +4,7 @@ from .forms import WineForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.views.generic import DeleteView
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -42,16 +43,20 @@ class All_wine(LoginRequiredMixin,ListView):
     paginate_by = 5
     queryset = Wine.objects.order_by('-id')
 
+class Detail_wine(LoginRequiredMixin,DetailView):
+    model = Wine
+    form_class = WineForm
+    template_name = 'wine/detail_wine.html'
 
 class Delete_wine(LoginRequiredMixin,DeleteView):
     model = Wine
-    template_name = 'wine/detail_wine.html'
+    template_name = 'wine/update_wine.html'
     success_url = reverse_lazy('wine:add')
 
 class Update_wine(LoginRequiredMixin,UpdateView):
     model = Wine
     form_class = WineForm
-    template_name = 'wine/detail_wine.html'
+    template_name = 'wine/update_wine.html'
     def get_success_url(self):
         args=self.object.id
         return reverse_lazy('wine:update', args=(self.object.id,))
