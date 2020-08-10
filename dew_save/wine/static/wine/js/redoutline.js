74168b98b926755e6a4f839058f1393fdbb0e7b2
atsushi-takahashi <67621987+atsushi-takahashi-0306@users.noreply.github.com>
2020-08-10 14:16:27 +0900

    ナビゲーションの色変え、画像ハイライト
    そのページにアクセスしたらナビゲーションの色が赤色にわかりやすくハイライトされるようにした。
    あとnew postsページで画像にホバーすると画像を浮き上がるようにした。

---
+++ dew_save/wine/static/wine/js/redoutline.js
@@ -0,0 +1,18 @@
+$(function () {
+
+
+     function red_list(url,list) {
+        $(window).bind('load', function () {
+            if(document.URL.match(url)) {
+                $(list).css('color', 'red');
+            }
+        });
+    }
+    red_list('all_wine/','.nav_list1');
+    red_list('my_wine/','.nav_list2');
+    red_list('add_wine/','.nav_list3');
+    red_list('update_user/','.nav_list4');
+
+});
+
+
