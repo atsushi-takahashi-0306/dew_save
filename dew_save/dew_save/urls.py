aa62db597bc281eba646d9c7c3054f8d3468ff83
atsushi-takahashi <67621987+atsushi-takahashi-0306@users.noreply.github.com>
2020-08-14 11:06:20 +0900

    細かなファイルの修正
    ファイルのこまかいところ修正。

--- dew_save/dew_save/urls.py
+++ dew_save/dew_save/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-path('admin/', admin.site.urls),
-path('',include('wine.urls')),
-path('',include('account.urls')),
-
+    path('admin/', admin.site.urls),
+    path('',include('wine.urls')),
+    path('',include('account.urls')),
 ]
